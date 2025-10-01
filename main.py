0e09c2f59db7d8ca41eeddfbec86b9114a996e82
Niraj Kumar <nkunwar30@hotmail.com>
2025-10-01 12:07:05 +0530

    regular changes

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+from tkinter import END
 from dotenv import load_dotenv
-import os
+from langchain_core.messages import HumanMessage
+from langgraph.graph import MessagesState, StateGraph
+from node import run_agent_reasoning, tool_node
 
 load_dotenv()
 
+AGENT_REASON = "Agent Reasoning"
+ACT = "Action"
+LAST = -1
+
+
+def should_continue(state: MessagesState) -> str:
+    """Decide whether to continue or end the flow."""
+    if not state["message"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow = StateGraph(MessagesState)
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {END: END, ACT: ACT})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
+
 if __name__ == "__main__":
-    print("Heallo ReACT Langgraph with Function Calling")
-  
+    print("Hello ReACT Langgraph with Function Calling")
---
+++ node.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+from react import llm, tools
+
+load_dotenv()
+
+SYSTEM_MESSAGE = """
+You are a helpful AI assistant that can use tools to answer questions
+"""
+
+
+def run_agent_reasoning(state: MessagesState) -> MessagesState:
+    """Run the reasoning node."""
+    response = llm.invoke([{"role": "system", "content": SYSTEM_MESSAGE}])
+    return {"message": [response]}
